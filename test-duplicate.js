c27100956e383c0ee4d6d54a767bee786446ebda
Christof Schramm <schrammc@Christofs-MBP.fritz.box>
2019-10-06 21:35:35 +0200

    untabify

--- test-duplicate.js
+++ test-duplicate.js
@@ -1,14 +1,14 @@
 function duplicateFunction(){
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
     alert("I'm a duplicate")
 
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
 
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
 }
--- test.js
+++ test.js
@@ -13,15 +13,15 @@ if (a = b.functionCall()) {
 
 function duplicateFunction(){
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
     alert("I'm a duplicate")
 
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
 
     for(var i = 0; i < 10; i++) {
-	console.log("I'm a duplicate");
+        console.log("I'm a duplicate");
     }
 }
