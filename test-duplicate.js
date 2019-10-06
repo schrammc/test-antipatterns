618f9aed99b633035144c2a308aa0310e700beba
Christof Schramm <schrammc@Christofs-MBP.fritz.box>
2019-10-06 21:33:23 +0200

    increase duplicate length

--- test-duplicate.js
+++ test-duplicate.js
@@ -1,5 +1,6 @@
 function duplicateFunction(){
     for(var i = 0; i < 10; i++) {
 	console.log("I'm a duplicate");
+	console.log("I'm a duplicate");
     }
 }
--- test.js
+++ test.js
@@ -14,5 +14,6 @@ if (a = b.functionCall()) {
 function duplicateFunction(){
     for(var i = 0; i < 10; i++) {
 	console.log("I'm a duplicate");
+	console.log("I'm a duplicate");
     }
 }
