ac48a55f1b3ee96a22eba678148ee22549febdda
Christof Schramm <schrammc@Christofs-MBP.fritz.box>
2019-10-06 21:34:47 +0200

    More stuff

--- test.js
+++ test.js
@@ -14,7 +14,14 @@ if (a = b.functionCall()) {
 function duplicateFunction(){
     for(var i = 0; i < 10; i++) {
 	console.log("I'm a duplicate");
-	console.log("I'm a duplicate");
     }
     alert("I'm a duplicate")
+
+    for(var i = 0; i < 10; i++) {
+	console.log("I'm a duplicate");
+    }
+
+    for(var i = 0; i < 10; i++) {
+	console.log("I'm a duplicate");
+    }
 }
